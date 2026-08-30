f14cadfc9932a18b0c0110cdfeb2129d18d21d88
dawud-shakir <dsshakir@gmail.com>
2026-08-30 10:24:45 -0600

    Add animated WebGL triangle homework

---
+++ hw1/triangle.js
@@ -0,0 +1,26 @@
+// --- Triangle ---
+/*
+const positions = new Float32Array([
+   0.0,  1.0, 0.0,  // vertex 1
+  -1.0, 0.0, 0.0,  // vertex 2
+   1.0, 0.0, 0.0   // vertex 3
+]);
+
+const colors = new Float32Array([
+  1.0, 0.0, 0.0,  // Red
+  0.0, 1.0, 0.0,  // Green
+  0.0, 0.0, 1.0   // Blue
+]);
+*/
+
+const positions = new Float32Array([
+   0.0,  0.8, 0.0,  // top
+  -0.6, -0.6, 0.0,  // bottom-left
+   0.8, -0.4, 0.0   // bottom-right
+]);
+
+const colors = new Float32Array([
+  0.8, 0.2, 1.0,  // Purple
+  1.0, 0.4, 0.0,  // Orange
+  0.0, 0.9, 1.0   // Cyan
+]);
