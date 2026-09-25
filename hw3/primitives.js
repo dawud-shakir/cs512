d5a5d56f0cd15433fc4c4b90ae055e5919a44906
dawud-shakir <dsshakir@gmail.com>
2026-09-25 16:23:04 -0600

    hw3: submission

--- hw3/primitives.js
+++ hw3/primitives.js
@@ -67,15 +67,15 @@ function createSphere(r = 1.0, vSteps = 20, uSteps = 20) {
             const z = r * sinu * sinv;
 
             // Alternate colors: R,G,B,K
-            rgb = [0.0, 0.0, 0.0];
-            a = rgb_offset % 4;
+            const rgb = [0.0, 0.0, 0.0];
+            const a = rgb_offset % 4;
             if (a < 3) {
                 rgb[a] = 0.25 + Math.max(0.0, cosv);
             }
-          
+
             colors.push(rgb[0], rgb[1], rgb[2]);
             rgb_offset++;
-            
+
             positions.push(x, y, z);
         }
     }
@@ -126,8 +126,8 @@ function createTorus(R = 1.0, r = 0.5, vSteps = 20, uSteps = 20) {
     }
 
     for (let i = 0; i <= vSteps; ++i) {
+        const c = (1 - Math.cos(i * 2 * Math.PI / vSteps)) / 2;
         for (let j = 0; j <= uSteps; ++j) {
-            c = i / vSteps;
             colors.push(c, c, c);
         }
     }
@@ -150,3 +150,113 @@ function createTorus(R = 1.0, r = 0.5, vSteps = 20, uSteps = 20) {
 }
 
 const torus = createTorus();
+
+// ====================
+// Cone
+// ====================
+
+function createCone(h = 1.0, r = 0.5, vSteps = 20, uSteps = 20) {
+    const positions = [];
+    const colors = [];
+    const indices = [];
+
+    for (let i = 0; i <= vSteps; ++i) {
+        const v = i / vSteps;
+
+        for (let j = 0; j <= uSteps; ++j) {
+            const u = j * 2.0 * Math.PI / uSteps;
+            const cosu = Math.cos(u);
+            const sinu = Math.sin(u);
+            const x = r * (1-v) * cosu;
+            const y = r * (1-v) * sinu;
+            const z = h * v;
+
+
+            positions.push(x, y, z);
+        }
+    }
+    const bottom = positions.length / 3;
+    positions.push(0, 0, 0);
+    var rgb = Array.from({length: 3}, () => Math.random());
+    for (let i = 0; i < positions.length / 3; ++i) {
+        const c = (1 - Math.cos(i * 2 * Math.PI / vSteps)) / 2;
+        colors.push(rgb[0] + c, rgb[1] + c, rgb[2] + c);
+    }
+
+    for (let i = 0; i < vSteps; ++i) {
+        for (let j = 0; j < uSteps; ++j) {
+            const k1 = (i * (uSteps + 1)) + j;
+            const k2 = k1 + uSteps + 1;
+
+            indices.push(k1, k2, k1 + 1);
+            indices.push(k2, k2 + 1, k1 + 1);
+        }
+    }
+    for (let j = 0; j < uSteps; ++j) {
+        indices.push(bottom, j + 1, j);
+    }
+
+    return {
+        positions: new Float32Array(positions),
+        colors: new Float32Array(colors),
+        indices: new Uint16Array(indices)
+    };
+}
+
+const cone = createCone();
+
+// ====================
+// Cylinder
+// ====================
+
+function createCylinder(h = 1.0, r = 0.5, vSteps = 20, uSteps = 20) {
+    const positions = [];
+    const colors = [];
+    const indices = [];
+
+    for (let i = 0; i <= vSteps; ++i) {
+        const v = i / vSteps;
+
+        for (let j = 0; j <= uSteps; ++j) {
+            const u = j * 2.0 * Math.PI / uSteps;
+            const cosu = Math.cos(u);
+            const sinu = Math.sin(u);
+            const x = r * cosu;
+            const y = r * sinu;
+            const z = h * v;
+
+            positions.push(x, y, z);
+        }
+    }
+    const bottom = positions.length / 3;
+    const top = bottom + 1;
+    positions.push(0, 0, 0, 0, 0, h);
+    var rgb = Array.from({length: 3}, () => Math.random());
+    for (let i = 0; i < positions.length / 3; ++i) {
+        const c = (1 - Math.cos(i * 2 * Math.PI / vSteps)) / 2;
+        colors.push(rgb[0] + c, rgb[1] + c, rgb[2] + c);
+    }
+
+    for (let i = 0; i < vSteps; ++i) {
+        for (let j = 0; j < uSteps; ++j) {
+            const k1 = (i * (uSteps + 1)) + j;
+            const k2 = k1 + uSteps + 1;
+
+            indices.push(k1, k2, k1 + 1);
+            indices.push(k2, k2 + 1, k1 + 1);
+        }
+    }
+    for (let j = 0; j < uSteps; ++j) {
+        const k1 = vSteps * (uSteps + 1) + j;
+        indices.push(bottom, j + 1, j);
+        indices.push(top, k1, k1 + 1);
+    }
+
+    return {
+        positions: new Float32Array(positions),
+        colors: new Float32Array(colors),
+        indices: new Uint16Array(indices)
+    };
+}
+
+const cylinder = createCylinder();
